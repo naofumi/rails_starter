9f166257579c7bb8530697cc470d00783d9d1662
Naofumi Kagami <naofumi@mac.com>
2022-02-13 15:42:41 +0900

    enable webconsole from other containers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # https://github.com/rails/web-console#configweb_consolepermissions
+  config.web_console.permissions = '172.0.0.0/8' # https://techzoom.net/lab/ip-address-calculator/
 end
