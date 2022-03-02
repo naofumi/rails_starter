1c6846bcea6f96fa73622118ef8bef5c03142f9a
Naofumi Kagami <naofumi@mac.com>
2022-03-02 23:42:29 +0900

    bin/rails g scaffold taggings post:references tag:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_143743) do
+ActiveRecord::Schema.define(version: 2022_03_02_144132) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name", null: false
@@ -19,10 +19,21 @@ ActiveRecord::Schema.define(version: 2022_03_02_143743) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_taggings_on_post_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "taggings", "posts"
+  add_foreign_key "taggings", "tags"
 end
