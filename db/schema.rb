e6df0a9e50db657c1dfcffe27dd6c31dae1b3013
Naofumi Kagami <naofumi@mac.com>
2022-03-02 23:38:34 +0900

    bin/rails g scaffold tag name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_143548) do
+ActiveRecord::Schema.define(version: 2022_03_02_143743) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name", null: false
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
