77b9e633cb4fe4c64a4fd9759118e7f49ead1515
Naofumi Kagami <naofumi@mac.com>
2022-03-03 09:11:54 +0900

    tag_names -> tag_names_string

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,15 @@ class Post < ApplicationRecord
   has_many :taggings, dependent: :restrict_with_error
   has_many :tags, through: :taggings, inverse_of: :posts
 
-  def tag_names=(tag_names)
-    self.tags = split_tag_names(tag_names)
+  def tag_names=(tag_names_string)
+    self.tags = split_tag_names(tag_names_string)
                   .map { |name| Tag.find_or_initialize_by(name: name) }
   end
 
 
   private
 
-    def split_tag_names(tag_names)
-      tag_names.split(/\s*,\s*/)
+    def split_tag_names(tag_names_string)
+      tag_names_string.split(/\s*,\s*/)
     end
 end
